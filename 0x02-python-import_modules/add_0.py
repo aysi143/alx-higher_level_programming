2e81b96c406b8081732c23f99815760ff43fb617
AYSI <aysanewyonasj@gmail.com>
2022-06-02 20:45:29 +0300

    args local change

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def add(a, b):
     """My addition function
-    pramametrs:
+
+    Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a + b
     """
