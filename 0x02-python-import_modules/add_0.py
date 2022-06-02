583d102373d167f4f29a550b980e0b94252e4971
AYSI <aysanewyonasj@gmail.com>
2022-06-02 20:40:39 +0300

    update module add_0.py

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -4,15 +4,7 @@ def add(a, b):
     pramametrs:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a + b
     """
     return (a + b)
-
-    """
-    The logic is simple!
-    The fucntion is used as a module that adds and returns sum of two numbers.
-
-    Then we are importing it into another python program to call this module.
-    """
