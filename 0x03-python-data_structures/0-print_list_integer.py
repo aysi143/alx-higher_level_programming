6c37c0004976689231bb0891a804869b347d2ad6
AYSI <aysanewyonasj@gmail.com>
2022-06-05 20:49:35 +0300

    road revert change

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+
 def print_list_integer(my_list=[]):
-        for aysi in my_list:
-                print("{:d}".format(aysi))
+    """
+    A function that prints all integers of a list.
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
