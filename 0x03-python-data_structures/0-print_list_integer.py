6fd0c93184a07b26c565361809684126e4991d52
AYSI <aysanewyonasj@gmail.com>
2022-06-05 07:27:00 +0300

    update |list printing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-        for elm in my_list:
-                print("{:d}".format(my_list[elm]))
+        for aysi in my_list:
+                print("{:d}".format(aysi))
