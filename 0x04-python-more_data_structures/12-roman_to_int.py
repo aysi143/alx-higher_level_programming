5dccfd20a213bd8f342f00854b9b1e3bd6cf1bba
AYSI <aysanewyonasj@gmail.com>
2022-06-08 22:17:01 +0300

    one pushing all locals

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    ns = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if ns[roman_string[i]] >= ns[roman_string[i + 1]]:
+                total += ns[roman_string[i]]
+            else:
+                total -= ns[roman_string[i]]
+            i += 1
+        total += ns[roman_string[i]]
+        return total
+    else:
+        return 0
