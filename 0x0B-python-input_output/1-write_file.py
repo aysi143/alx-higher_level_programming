65a7ff0b203ceabe60740f90a5c5a6779df17256
Aysanew Yonas <49403558+aysi143@users.noreply.github.com>
2022-07-05 22:39:08 +0300

    its ok to fail once | debug locally

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for write_file method.
+"""
+
+
+def write_file(filename="", text=""):
+    """write_file writes a string to a text file.
+    Args:
+        filename (str): name of file.
+        text (str): text to be written.
+    Return: number of bytes written.
+    """
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        return (f.write(text))
